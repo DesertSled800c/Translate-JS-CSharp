498e5db99cd38233855e03f9cf9ca1757ef02acb
Akshay Khona <desertsled800c@gmail.com>
2023-02-24 14:21:39 -0600

    PlaceNamesDone

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -10,3 +10,16 @@ console.log("All Pace Names");
 for (const name of names) {
   console.log(name);
 }
+
+console.log("")
+
+const theNames = (names) => {
+  console.log(`'The' Place Names`)
+  names.map((name) => {
+   if (name.startsWith("The")) {
+     console.log(name)
+   }
+  })
+}
+
+theNames(names)
